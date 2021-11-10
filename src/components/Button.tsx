967f127d9463169dda13ba6cb61853b2e8f05710
Camila <ccvieira@ice.ufjf.br>
2021-11-10 09:35:58 -0300

    button import types

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,7 @@
-export function Button() {
-  return <button>clique aqui</button>;
+type ButtonProps = {
+  children?: string;
+};
+
+export function Button(props: ButtonProps) {
+  return <button>{props.children || "Default"}</button>;
 }
