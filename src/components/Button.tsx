52040967f3bee2d1c6083a5de5d586119677f6f9
Camila <ccvieira@ice.ufjf.br>
2021-11-10 11:26:45 -0300

    saving due a new pbi

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,13 @@
-type ButtonProps = {
-  children?: string;
-};
+import { useState } from "react";
 
-export function Button(props: ButtonProps) {
-  return <button>{props.children || "Default"}</button>;
+export function Button() {
+  //let counter = 0;
+  const [counter] = useState(0);
+
+  function increment() {
+    //counter += 1;
+    console.log(counter);
+  }
+
+  return <button onClick={increment}>{counter}</button>;
 }
